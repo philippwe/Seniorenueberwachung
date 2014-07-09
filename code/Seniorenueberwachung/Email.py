454aa6472ef6c8f769e84840ebf0cb7cd60cde6c
philippwe <pwilken@web.de>
2014-07-09 17:21:56 +0200

    Email-Skript abgeschlossen! Das Passwort ist in der Doku zu finden

--- code/Seniorenueberwachung/Email.py
+++ code/Seniorenueberwachung/Email.py
@@ -1,21 +1,32 @@
 import smtplib
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
-import time
+COMMASPACE = ","
 
-absender = "pwilken@web.de"
-adressat ="philipp.wilken@accenture.com"
-betreff = "test"
-inhalt = "Hallo , ich bin Pi"
-zeit = time.ctime(time.time())
+print "Email-Skript starten"
+adressat = ["pwilken@web.de", "philipp.wilken@accenture.com"]
+inhalt = "Hallo, das Ueberwachungssystem hat einen Alarm ausgeloest. Bitte kontaktieren Sie sofort die ueberwachte Person!"
 
-server = smtplib.SMTP("smtp.web.de", 587)
-server.set_debuglevel(1)
+msg = MIMEMultipart()
+msg["Subject"] = "Alarm"
+msg["From"]= "ueberwachungraspberry@gmail.com"
+msg["To"]= COMMASPACE.join(adressat) 
+emailinhalt = MIMEText(inhalt, "plain")
+msg.attach(emailinhalt)
 
+print "Port setzen"
+server = smtplib.SMTP("smtp.gmail.com", 587)
+
+print "Verschluesselung"
 server.starttls()
-server = smtplib.SMTP("smtp.web.de")
-server.login("pwilken", ".")
 
-server.sendmail("pwilken@web.de", "pwilken@web.de", "test from Pi")
+print "anmelden"
+server.login("ueberwachungraspberry", "xxxPasswort siehe Dokuxxx") #Darauf achten das das Passwort beim Commit raus ist!!!
 
+print "Email verschicken"
+server.sendmail("ueberwachungraspberry@gmail.com", adressat, msg.as_string())
 
+print "Serververbindung schliessen"
 server.quit()
