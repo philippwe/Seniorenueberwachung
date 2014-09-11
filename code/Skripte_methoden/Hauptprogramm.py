87928d9a8a12ab39cbd66aca4f5db48abe69a61b
pasvadin <pasvadin@live.de>
2014-09-11 15:31:40 +0200

    änderungen von gestern

--- code/Skripte_methoden/Hauptprogramm.py
+++ code/Skripte_methoden/Hauptprogramm.py
@@ -1,6 +1,4 @@
 ########### globale Variablen 
-
-
 #Auf diese koennen die Methoden zugreifen
 
 gv_stopCheckCard = 0
@@ -23,15 +21,16 @@ def getCheckTime():  #liest den Zeitraum aus, in welcher sich die ueberwachte Pe
 	fobj.close()
 	
 	countmax =  int(s[1].strip())  #entfernt leerzeichen
-	#countmax = countmax * 60
+	countmax = countmax * 60
 	return countmax
 
 def getNewest(iv_path):
 
 	import os
 	import glob
+
 	try:
-		newest = max(glob.iglob(iv_path+"/*.*"), key = os.path.getctime)
+		newest = max(glob.iglob(iv_path+"/*.*"), key = os.path.getctime) #der Name des neuesten Bildes wird ausgelesen
 		
 	except ValueError:
 		return 0
@@ -49,9 +48,11 @@ def checkChange(iv_counter, iv_pfad, id):  #ueberprueft ob dateien dazu gekommen
 	anzahl = 0
 	counter = 0
 	
+	#globale Variablen zum Beenden der Methode:
 	global gv_stopCheckChangeConfirm
 	global gv_checkChangeReturn
 	global gv_stopCheckChange
+	#globale Variable, die gesetzt wird, wenn ein neues Bild erkannt wurde
 	global gv_newImage
 	
 	gv_stopCheckChange = 0
@@ -62,12 +63,12 @@ def checkChange(iv_counter, iv_pfad, id):  #ueberprueft ob dateien dazu gekommen
 	while (gv_checkChangeReturn == 0) and (gv_stopCheckChange == 0):
 				
 		time.sleep(1)
-		objects = os.listdir(iv_pfad)
+		objects = os.listdir(iv_pfad) #objekte im Ordner werden ausgelesen
 		curr_anzahl = len(objects)
 			
 		if anzahl == 0:
 			anzahl = curr_anzahl
-		#fuer den fall, dass bilder geloescht wurden
+		#fuer den fall, dass bilder geloescht wurden ist die Zahl der Bilder im Ordner niedriger als der zaehler
 		if curr_anzahl < anzahl:
 			anzahl = curr_anzahl
 			
@@ -77,10 +78,10 @@ def checkChange(iv_counter, iv_pfad, id):  #ueberprueft ob dateien dazu gekommen
 			gv_newImage = 1
 		else:
 			counter = counter +1
-			print id+": "+str(counter) #todo noch entfernen
+			print id+": "+str(counter)
 			
-		if counter == iv_counter:  #10:  #30	
-			gv_checkChangeReturn = 1 #lange zeit keine bewegung -> benachrichtigung
+		if counter == iv_counter:	
+			gv_checkChangeReturn = 1 #wenn laengere zeit keine bewegung erfolgt, wird eine benachrichtigung abgeschickt
 
 	print "--------------- returning from checkChange"
 	
@@ -92,11 +93,14 @@ def periodicCleanup(iv_deletePeriod):
 	import delete
 	import time
 
+	#globale Variablen zum Beenden der Methode:
 	global gv_stopPeriodicCleanup
 	global gv_stopPeriodicCleanupConfirm
+	
 	gv_stopPeriodicCleanupConfirm = 0
 	gv_stopPeriodicCleanup = 0
 	
+	#falls die Methode von Aussen beendet wird, wird auf die globale Variable geprueft
 	while (gv_stopPeriodicCleanup == 0):
 		
 		if (time.localtime()[4]%10)==5:
@@ -107,10 +111,13 @@ def periodicCleanup(iv_deletePeriod):
 	return
 
 def checkCard(iv_path): #return: 0 = nichts; 1 = rot; 2 = gelb;
+	
 	import Pixel
 	
-	global gv_newImage
-	global gv_checkCardReturn
+	#globale Variablen 	
+	global gv_newImage #wenn ein neues Bild erstellt wurde
+	global gv_stopCheckCard
+	global gv_checkCardReturn 
 	
 	while (gv_stopCheckCard == 0):
 		try:			
@@ -122,12 +129,13 @@ def checkCard(iv_path): #return: 0 = nichts; 1 = rot; 2 = gelb;
 				if (path !=  0):
 					returnCard = Pixel.bildauslesen(path)
 					if returnCard == 1:
-						gv_checkCardReturn = 1
+						gv_checkCardReturn = 1 #eine rote Karte wurde erkannt
+
 						#elseif (pixelmethode == "gelb":)
 						#gv_checkCardReturn = 2	
 						break
 					else:  
-						gv_checkCardReturn = 0
+						gv_checkCardReturn = 0 #keine farbige Karte wurde erkannt
 					
 				gv_newImage = 0
 		
@@ -136,7 +144,6 @@ def checkCard(iv_path): #return: 0 = nichts; 1 = rot; 2 = gelb;
 		except IndexError:
 			returnCard = 0
 		
-
 	return #es sollte ja nicht nachdem ein bild geprueft wird gleich aus der methode rausgegangen werden. so wird der thread erst beendet, wenn von aussen ein stopCheckCard kommt
 
 def ueberwachen(iv_path):  #main
@@ -146,18 +153,20 @@ def ueberwachen(iv_path):  #main
 	import thread
 	import Email	 #Email.py
 	import Signalton #Signalton.py
-	
-	
+		
 	#Ueberwachungsdauer auslesen
 	countmax = getCheckTime()
 	
+	#globale Variablen zum Beenden von Methoden in anderen Threads:
 	global gv_stopCheckChange
 	global gv_checkChangeReturn	
 	global gv_stopCheckChangeConfirm
 	global gv_checkCardReturn
-	alarmMailSent = 0	
+
+	alarmMailSent = 0 #fuer eine spaetere Deaktivierung eines ausgeloesten Alarms 	
 	
 	try: 
+		#die benoetigten Threads werden gestartet:
 		thread.start_new_thread(checkChange, (countmax, iv_path, "anfang"))
 		thread.start_new_thread(checkCard, (iv_path,))
 		thread.start_new_thread(periodicCleanup, (10,))
@@ -172,11 +181,9 @@ def ueberwachen(iv_path):  #main
 				while (gv_stopCheckChangeConfirm == 0):
 					pass   #damit der thread auch wirklich beendet ist
 								
-				thread.start_new_thread(checkChange, (25, iv_path, "keine bewegung")) #1 thread
-				#time.sleep(30)
+				thread.start_new_thread(checkChange, (25, iv_path, "keine bewegung"))
 				c = 0
 				while (c<=30):
-				#	print "t0: "+str(c)
 					time.sleep(1)
 					if (gv_checkCardReturn == 1):
 						break #mail verschicken
@@ -204,6 +211,8 @@ def ueberwachen(iv_path):  #main
 					
 					thread.start_new_thread(checkChange, (countmax, iv_path, "abbruch/anfang"))
 			
+			############ Im Folgenden wird die Moeglichkeit einer weiteren Karte zum Pausieren der Ueberwachung eingefuehrt
+
 			#if (gv_checkCardReturn == 2) and (alarmMailSent == 1):  #wenn eine gelbe karte vorgehalten wird, wird der alarm unterbrochen
 				#thread.start_new_thread(checkChange, countmax, iv_path)
 				#Email.sendMail("Fehlalarm") #todo parameter einfuegen
@@ -219,7 +228,7 @@ def ueberwachen(iv_path):  #main
 					#gv_checkCardReturn = 0
 					#pause = 0
 			
-			if (gv_checkCardReturn == 1) and (alarmMailSent == 0): #rote karte
+			if (gv_checkCardReturn == 1) and (alarmMailSent == 0): #rote Karte erkannt
 				print "-------- Rotgefunden"
 				#Signalton.output("kontrolle") #muss noch erstellt werden
 				if (gv_checkCardReturn == 1):
@@ -227,14 +236,14 @@ def ueberwachen(iv_path):  #main
 					alarmMailSent = 1
 				gv_checkCardReturn = 0
 				#gv_stopCheckChange = 1
-				break #kann spaeter entfernt werden. 				
-		time.sleep(10)
+				break 				
+
 	except KeyboardInterrupt:
 		print ("Programm beendet")
-		#Signalton.output("programmBeendet") TODO
+		#Signalton.output("programmBeendet")
 		
 	except:
 		print ("Fehler:" +str(sys.exc_info()[0]))
-		#Signalton.output("Fehler") TODO
+		#Signalton.output("Fehler")
 		raise
 		
